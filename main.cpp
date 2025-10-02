6d143a916a5b3d20a81d991e5a5e930ce335479f
pbsmfs <fsmart2006@gmail.com>
2025-10-02 13:03:32 +0300

    conflict resolved

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+using namespace std;
 int main() {
+	char name[20];
 	std::cout << "Hello world! Have a nice day!\n";
+	cout << "Enter your name: ";
+	cin >> name;
+	cout << "Hello, " << name << "!\n";
+	cout << "Nice to see you learning Git\n";
 	system("pause");
 	return 0;
 }
