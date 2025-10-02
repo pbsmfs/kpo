c94f02963cbb30fb74ea24a9e981d6b868bc5505
pbsmfs <fsmart2006@gmail.com>
2025-10-02 14:00:09 +0300

    test commit

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main() {
 	char name[20];
 	cout << "Enter your name: ";
 	cin >> name;
-	cout << "Hello, " << name << "!\n";
+	cout << "Hello, " << name << "!!\n";
 	cout << "Nice to see you learning Git\n";
 	system("pause");
 	return 0;
