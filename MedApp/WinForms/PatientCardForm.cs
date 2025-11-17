0bcb40f844b998c449c163dda66e5f025349e95c
Beekonchik <sh1njajamao@gmail.com>
2025-11-17 16:47:57 +0300

    PatientCard form

---
+++ MedApp/WinForms/PatientCardForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms
+{
+    public partial class PatientCardForm : Form
+    {
+        public PatientCardForm()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label_Form_Name_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
